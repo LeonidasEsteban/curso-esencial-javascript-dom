091d6d83261ad9d327f8bb4d3dab9e25455b68ae
Leonidas Esteban <leonidasesteban@gmail.com>
2021-08-24 00:06:13 -0500

    clase 9 strings

---
+++ index.js
@@ -0,0 +1,21 @@
+/* Strings */
+
+'Leonidas'
+
+"Leonidas"
+String(5) // ?
+// `Leonidas`
+
+const name = 'Leonidas'
+const lastname = 'Esteban'
+
+// const fullname = name + lastname //?
+// const fullname = name + ' ' + lastname //?
+const fullname = `${name} ${lastname}` // ?
+
+fullname.length //?
+fullname.charAt(3) //?
+fullname.replace('Leonidas', 'Rivaldo') // ?
+fullname.search('xxxxx') // ?
+fullname.toUpperCase() // ?
+

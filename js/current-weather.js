b448e4ec033ce20431dac9ca47d3996ff2a4e9ad
Leonidas Esteban <leonidasesteban@gmail.com>
2021-08-31 21:29:31 -0500

    clase 27 - Renderizando la ciudad origen, DOM Elements

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,10 +1,18 @@
 import weather from '../data/current-weather.js'
+// weather.name //?
+
+function setCurrentCity($el, city) {
+  $el.textContent = city
+}
 
 function configCurrentWeather(weather) {
 
   // loader
   // date
   // city
+  const $currentWeatherCity = document.querySelector('#current-weather-city')
+  const city = weather.name
+  setCurrentCity($currentWeatherCity, city)
   // temp
   // background
 

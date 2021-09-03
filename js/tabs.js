f192aa729f40b15f65e7b2d22af34bae8dc80c5d
Leonidas Esteban <leonidasesteban@gmail.com>
2021-09-03 17:27:01 -0500

    clase 41 - Navegación entre tabs

--- js/tabs.js
+++ js/tabs.js
@@ -23,6 +23,7 @@ function nextDay(day) {
 }
 
 $tabList.forEach(($tab, index) => {
+  $tab.addEventListener('click', handleSelectTabClick)
   if (index === 0) {
     $tab.textContent = 'Hoy'
     weekday = nextDay(weekday)
@@ -31,3 +32,18 @@ $tabList.forEach(($tab, index) => {
   $tab.textContent = week[weekday]
   weekday = nextDay(weekday)
 })
+
+
+function handleSelectTabClick(event) {
+  const $tabSelected = event.target
+  const $tabActive = document.querySelector('.tab[aria-selected="true"]')
+  $tabActive.removeAttribute('aria-selected')
+  $tabSelected.setAttribute('aria-selected', true)
+
+  const id = $tabSelected.id
+  const $tabPanel = document.querySelector(`[aria-labelledby=${id}]`)
+  const $tabPanelSelected = document.querySelector(`.tabPanel:not([hidden])`)
+  $tabPanel.hidden = false
+  $tabPanelSelected.hidden = true
+  // debugger
+}
